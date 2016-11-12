270d0244fa4164095ea267032671baecdea56d00
Masiur Rahman Siddiki <masiur.sust@csesociety.org>
2016-11-12 19:35:31 +0600

    name added

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 	<title>Workshop || Instructor</title>
+  <!-- Masiur Rahman Siddiki -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
